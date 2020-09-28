ad951f92b13d3e3d5173a276798867db199bf7e2
Clint Losee <clint.losee@gmail.com>
2020-09-28 08:33:21 -0600

    adding files

---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BeersPage() {
+  return (
+    <>
+      <p>Hi! I'm the beers page!</p>
+    </>
+  );
+}
---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <>
+      <p>Hi! I'm the order page!!!</p>
+    </>
+  );
+}
