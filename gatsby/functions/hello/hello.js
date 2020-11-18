d9caa4d90a2bb0e552b1d4915f8b2ac8cceee171
Clint Losee <clint.losee@gmail.com>
2020-11-18 16:41:27 -0700

    Added example for Vercel serverless function

--- gatsby/functions/hello/hello.js
+++ gatsby/functions/hello/hello.js
@@ -1,4 +1,11 @@
+//* Netlify serverless function
 exports.handler = async (event, context) => ({
   statusCode: 200,
   body: 'Hello!',
 });
+
+//* Vercel serverless function
+// module.exports = async (req, res) => ({
+//   statusCode: 200,
+//   body: 'Hello from Vercel!',
+// });
